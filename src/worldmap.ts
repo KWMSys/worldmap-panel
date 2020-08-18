e01f9ddad5a17cae1dc78b7328947c098d3d97af
Marek Schmidt <info@msdev.me>
2020-08-18 02:33:56 +0200

    Add Map providers

--- src/worldmap.ts
+++ src/worldmap.ts
@@ -17,6 +17,18 @@ const tileServers = {
       '&copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
     subdomains: 'abcd',
   },
+  'OpenTopoMap': {
+    url: 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+    attribution:
+      'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+    subdomains: 'abc',
+  },
+  'OpenStreetMap DE': {
+    url: 'https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png',
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    subdomains: 'abc',
+  },
 };
 
 export default class WorldMap {
